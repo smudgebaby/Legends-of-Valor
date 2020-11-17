0a5ee862d71760610f2be1cd40a0327b9ff41a75
Xinyi Zhao <zxy1307@bu.edu>
2020-11-17 18:08:01 -0500

    finish game select

--- src/GameSelect.java
+++ src/GameSelect.java
@@ -1,15 +1,52 @@
 package src;
 
+import java.util.Scanner;
+
 public class GameSelect {
+	static Scanner in = new Scanner(System.in);
+	
     public static void main(String[] args) {
         //Effect:Begins logic for starting game select
         //Something like selectGame().start()
-        MonsterANDHeroes game = new MonsterANDHeroes();
-        game.start();
+    	
+        //MonsterANDHeroes game = new MonsterANDHeroes();
+        //game.start();
+        selectGame();
+        
     }
 
-    public Game selectGame(){
+    public static void selectGame(){
         //Effect: User Selects which game to play, MAH or LOV and starts Game
         //Returns: Game Selected
+    	
+    	System.out.println("We have two games: [1] Legends: Monsters and Heroes");
+    	System.out.println("		[2] Legends of Valor");
+    	System.out.print("Please enter a number to choose from games: ");
+    	
+    	in.nextLine();
+    	int gameNum = isInt();
+    	while (gameNum!=1||gameNum!=2) {
+    		System.out.println("Invalid input! Please enter a number to choose from 2 games: ");
+    		in.nextLine();
+    		gameNum = isInt();
+    	}
+    	if(gameNum==1) {
+    		MonsterANDHeroes game = new MonsterANDHeroes();
+            game.start();
+    	}else {
+    		LegendsOfValor game = new LegendsOfValor();
+    		game.start();
+    	}
+         
     }
+    
+    
+    private static int isInt() {
+		while(!in.hasNextInt()) {
+			System.out.print("Invalid input! Please enter a number: ");
+			in.nextLine();
+		}
+		int n = in.nextInt();
+		return n;
+	}
 }
